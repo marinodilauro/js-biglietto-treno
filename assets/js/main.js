79428bf855da46cf4e6b74bb25a715102c0f96da
marinodilauro <m.dilauro90@gmail.com>
2024-02-19 16:01:16 +0100

    Add variables for user age and travel distance and assign them values from user inputs

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Creo due variabili per l'età dell'utente e i km che deve percorrere
+// Assegno a queste due variabili dei valori tramite input utente
+// Trasformo il tipo di variabile da stringa a numero
+
+const userAge = Number(prompt("Inserisca l'età del passeggero"));
+console.log(userAge);
+
+const travelDistance = Number(prompt("Inserisca la distanza da percorrere (in km)"));
+console.log(kmToTravel);
+

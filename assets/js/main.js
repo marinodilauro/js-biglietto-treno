5d15a11bf01c3b3ed4eb1828ae26c26b488542b1
marinodilauro <m.dilauro90@gmail.com>
2024-02-19 16:04:54 +0100

    Add constant for ticket base price and variable for ticket final price. Fix travel distance variable name in console.log()

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,14 @@ const userAge = Number(prompt("Inserisca l'età del passeggero"));
 console.log(userAge);
 
 const travelDistance = Number(prompt("Inserisca la distanza da percorrere (in km)"));
-console.log(kmToTravel);
+console.log(travelDistance);
 
+// Creo una costante per il prezzo al km del biglietto (0.21€)
+
+const ticketBasePrice = 0.21;
+console.log(ticketBasePrice);
+
+// Creo una varaibile e le assegno il prezzo del biglietto dell'utente (sarà 0.21 moltiplicato il valore della variabile "km da percorrere")
+
+let ticketFinalPrice = ticketBasePrice * travelDistance;
+console.log(ticketFinalPrice);
